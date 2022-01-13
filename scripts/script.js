91b1470cd18da7754e53bf320292d379352b9253
Andrey E <koman499@gmai.com>
2022-01-13 21:37:47 +0300

    открытие и закрытие модального окна

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,14 @@
-console.log('Hello, world!');
+'use strict';
+const modalAdd = document.querySelector('.modal__add'),
+      addAd = document.querySelector('.add__ad');
+
+addAd.addEventListener('click', () => {
+  modalAdd.classList.remove('hide');
+});
+
+modalAdd.addEventListener('click', (event) => {
+  const target = event.target;
+  if(target.classList.contains('modal__close') || target === modalAdd) {
+    modalAdd.classList.add('hide');
+  }
+});
