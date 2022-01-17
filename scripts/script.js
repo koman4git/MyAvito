1f366b811b65abfc471c00057bca2035682a389e
Andrey E <koman499@gmai.com>
2022-01-17 21:02:31 +0300

    написана общая функция закрытия модального окна (closeModal) при клике на крестик, при клике мимо окна (на подложку)

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,43 @@
 'use strict';
+//disabled - блокировка кнопки
+//reset() перезагрузка формы
+//Метод Element.closest() возвращает ближайший родительский элемент (или сам элемент), который соответствует заданному CSS-селектору или null, если таковых элементов вообще нет.
+
 const modalAdd = document.querySelector('.modal__add'),
-      addAd = document.querySelector('.add__ad');
+      addAd = document.querySelector('.add__ad'),
+      modalBtnSubmit = document.querySelector('.modal__btn-submit'),
+      modalSubmit = document.querySelector('.modal__submit'),
+      catalog = document.querySelector('.catalog'),
+      modalItem = document.querySelector('.modal__item')
+
+// const closeModal = event => {
+//   const target = event.target;
+//   if(target.classList.contains('modal__close') || target === modalAdd || target === modalItem) {
+//     modalAdd.classList.add('hide');
+//     modalItem.classList.add('hide');
+//     modalSubmit.reset();
+//   }    
+// };
+const closeModal = function (event) {
+  const target = event.target;
+  if(target.closest('.modal__close') || target === this) {
+    this.classList.add('hide');
+    modalSubmit.reset();
+  }
+}
 
 addAd.addEventListener('click', () => {
   modalAdd.classList.remove('hide');
+  modalBtnSubmit.disabled = true;//disabled - блокировка кнопки
 });
 
-modalAdd.addEventListener('click', (event) => {
+modalAdd.addEventListener('click', closeModal);
+modalItem.addEventListener('click', closeModal);
+
+catalog.addEventListener('click', event => {
   const target = event.target;
-  if(target.classList.contains('modal__close') || target === modalAdd) {
-    modalAdd.classList.add('hide');
+  if(target.closest('.card')){
+    modalItem.classList.remove('hide');
   }
 });
+
